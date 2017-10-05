50ba300697e9d36e91397158ad1dadc573a96567
JohnGaltt <oleksandr.zholob.kn.2016@lpnu.ua>
2017-10-05 07:54:13 +0300

    My and oleg project for hackaton init

---
+++ ProjectHackton/TestBestHack/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using TestBestHack.Models;
+using Newtonsoft.Json;
+
+namespace TestBestHack.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public RootObject GetData(string category)
+        {
+            WebRequest request = WebRequest.Create(@"https://newsapi.org/v1/sources?category=" + category + "&language=en");
+
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            WebResponse response = request.GetResponse();
+
+            string answer = String.Empty;
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    answer = reader.ReadToEnd();
+                }
+            }
+
+            response.Close();
+
+            RootObject obj = JsonConvert.DeserializeObject<RootObject>(answer);
+
+            return obj;
+        }
+
+        [HttpPost]
+        public ActionResult Index(string category)
+        {
+            //string[] str = Enum.GetNames(typeof(CategoryType));
+
+            HttpContext.Response.Headers.Add("refresh", "7200; url=" + Url.Action("Index"));
+
+            CategoryType enumDisplayStatus = (CategoryType)Int32.Parse(category);
+            string stringValue = enumDisplayStatus.ToString();
+
+            RootObject obj =  GetData(stringValue);
+
+            List<Source> sources = new List<Source>();
+
+            foreach (var s in obj.sources)
+            {
+                sources.Add(s);
+            }
+
+            return View(sources);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //HttpContext.Response.Headers.Add("refresh", "7200; url=" + Url.Action("Index"));
+
+            RootObject obj = GetData("business");
+
+            List<Source> sources = new List<Source>();
+
+            foreach (var s in obj.sources)
+            {
+                sources.Add(s);
+            }
+
+            return View(sources);
+        }
+    }
+}
---
+++ ProjectHackton/TestBestHack/Models/Source.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestBestHack.Models
+{
+    public class Source
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public CategoryType Category { get; set; }
+        public string language { get; set; }
+        public string country { get; set; }
+        public UrlsToLogos urlsToLogos { get; set; }
+        public List<string> sortBysAvailable { get; set; }
+    }
+
+    public enum CategoryType
+    {
+        business,
+        entertainment,
+        gaming,
+        general,
+        music,
+        politics,
+        sport,
+        technology
+    }
+}
